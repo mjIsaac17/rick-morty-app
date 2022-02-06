2855c8f213978f68dc0c7351bebde2bf46267302
Isaac <theisaacmj@gmail.com>
2022-02-06 13:39:40 -0800

    Reset pagination when switching between pages

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,17 +1,28 @@
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import Box from '@mui/material/Box'
 import Drawer from '@mui/material/Drawer'
 import List from '@mui/material/List'
 import Divider from '@mui/material/Divider'
 import ListItem from '@mui/material/ListItem'
-import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
+import Typography from '@mui/material/Typography'
+
+import ListItemIcon from '@mui/material/ListItemIcon'
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt'
+import HomeIcon from '@mui/icons-material/Home'
 import LiveTvIcon from '@mui/icons-material/LiveTv'
-import { Typography } from '@mui/material'
+import { useDispatch } from 'react-redux'
+import { setCurrentPage } from '../../actions/pagination.action'
 
 const Sidebar = ({ isOpen, handleToogleSidebar }) => {
   console.log('<Sidebar>')
+  const history = useHistory()
+  const dispatch = useDispatch()
+
+  const handleNavigate = (path) => {
+    dispatch(setCurrentPage(1))
+    history.push(path)
+  }
 
   return (
     <>
@@ -29,18 +40,24 @@ const Sidebar = ({ isOpen, handleToogleSidebar }) => {
               </ListItemText>
             </ListItem>
             <Divider />
-            <Link to='/character'>
+            <Link to='/'>
+              <ListItem button>
+                <ListItemIcon><HomeIcon /> </ListItemIcon>
+                <ListItemText primary='Home' />
+              </ListItem>
+            </Link>
+            <div onClick={() => handleNavigate('/character')}>
               <ListItem button>
                 <ListItemIcon><PeopleAltIcon /> </ListItemIcon>
                 <ListItemText primary='Characters' />
               </ListItem>
-            </Link>
-            <Link to='/episode'>
+            </div>
+            <div onClick={() => handleNavigate('/episode')}>
               <ListItem button>
                 <ListItemIcon><LiveTvIcon /> </ListItemIcon>
                 <ListItemText primary='Episodes' />
               </ListItem>
-            </Link>
+            </div>
           </List>
         </Box>
       </Drawer>
